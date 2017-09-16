6f4898935a4acc2c400354cdb32fef1cb0c1773a
Howard Passmore <howard.passmore17@gmail.com>
2017-09-16 00:02:32 -0700

    Restructure and add lastpass

--- macSetup.sh
+++ macSetup.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
-
 #Get homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-#Setup oh-my-zsh
-brew install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp zshrc ~/.zshrc
-
 #Setup Git
-echo .DS_Store >> ~/.gitignore_global
+cp gitignore ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
 
 #Setup for vim
@@ -17,51 +11,120 @@ brew install vim
 cp vimrc ~/.vimrc
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
 git clone https://github.com/jiangmiao/auto-pairs.git ~/.vim/bundle/auto-pairs
 git clone https://github.com/alvan/vim-closetag.git ~/.vim/bundle/vim-closetag
 
-#Setup Golang
-mkdir ~/go
-mkdir ~/go/src
-mkdir ~/go/bin
-mkdir ~/go/pkg
-brew install go
-
-#Setup haskell
-brew cask install haskell-platform
-mkdir ~/Haskell
+#Setup VSCode
+brew cask install visual-studio-code
 
 #Setup java
 brew cask install java
+code --install-extension redhat.java
 source ~/.zshrc
 
-#Setup Octave/MATLAB
-brew tap homebrew/science
-brew cask install xquartz
-brew install octave
-
-#Install useful programs
+#Install useful programs I always use
 brew cask install eclipse-ide 
-brew cask install visual-studio-code
 brew cask install iterm2
 brew cask install textual
-brew cask install teamviewer
 brew cask install the-unarchiver
 brew cask install google-chrome
 brew cask install firefox
-brew cask install google-drive
-brew cask install battle-net
-brew cask install proxpn
-brew cask install amazon-music
 brew install maven
 brew install gradle
-brew cask install android-studio
-brew cask install android-sdk
-brew cask install android-ndk
 brew cask install virtualbox
 brew cask install virtualbox-extension-pack
-brew cask install vmware-fusion
-brew cask install microsoft-office
-brew install npm
-npm install -g jshint
+
+#Setup Golang
+while true; do
+    read -p "Do you wish to setup golang?" golang
+    case $golang in
+        [Yy]* ) mkdir ~/go
+                mkdir ~/go/src
+                mkdir ~/go/bin
+                mkdir ~/go/pkg
+                git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
+                brew install go
+                code --install-extension lukehoban.go
+                break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+
+#Setup haskell
+while true; do
+    read -p "Do you wish to setup haskell?" haskell
+    case $haskell in
+        [Yy]* ) brew cask install haskell-platform
+                mkdir ~/Haskell
+                code --install-extension justusadam.language-haskell
+                break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+#Setup Javascript development
+while true; do
+    read -p "Do you wish to setup useful Javascript tools (npm, jshint, etc..)?" javascript
+    case $javascript in
+        [Yy]* ) code --install-extension dbaeumer.jshint
+                brew install npm
+                npm install -g jshint
+                break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+#Setup Android studio and Android libraries
+while true; do
+    read -p "Do you wish to setup Android development tools?" android
+    case $android in
+        [Yy]* ) brew cask install android-studio
+                brew cask install android-sdk
+                brew cask install android-ndk
+                break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+#Setup Octave/MATLAB
+while true; do
+    read -p "Do you wish to setup Octave/MATLAB?" octave
+    case $octave in
+        [Yy]* ) brew tap homebrew/science
+                brew cask install xquartz
+                brew install octave
+                break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+#Only do these on a personal (IE non-work machine)
+while true; do
+    read -p "Do you wish to setup personal use programs (games, office, etc..)?" personal
+    case $personal in
+        [Yy]* ) brew cask install teamviewer
+                brew cask install battle-net
+                brew cask install proxpn
+                brew cask install amazon-music
+                brew cask install vmware-fusion
+                brew cask install microsoft-office
+                break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+#Setup lastpass
+brew cask install lastpass
+open /usr/local/Caskroom/lastpass/latest/LastPass\ Installer.app
+
+#Setup oh-my-zsh
+brew install zsh
+cp zshrc ~/.zshrc
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
