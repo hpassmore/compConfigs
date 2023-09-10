d7b4b5f5687ddd40573c532e3b648946418de339
Howard Passmore <holojack123@gmail.com>
2023-09-09 23:37:17 -0400

    Add steam to gaming setup

--- linuxSetup_endeavouros.bash
+++ linuxSetup_endeavouros.bash
@@ -79,7 +79,7 @@ while true; do
                     lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo \
                     sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama \
                     ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 lutris\
-                    lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
+                    lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader steam
                 while true; do
                     echo
                     read -p "Does computer contain an NVIDIA graphics card? (y/n)" nvda
